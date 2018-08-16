0e89b1c9142f4e03fe38b45ff4de4d3a202881f7
NilsBrehm <hd280kilxtc>
2018-08-16 17:04:00 +0200

    some more stats for call series

--- save_call_series_to_py.m
+++ save_call_series_to_py.m
@@ -15,15 +15,25 @@ for i = 3:length(listing)
     end
 end
 %% Get Pulse Times and Call Duration
-samples = cell(1, length(recs));
-call_dur = cell(1, length(recs));
+samples = cell(length(recs), 1);
+call_dur = cell(length(recs), 1);
+p_rate = zeros(length(recs), 1);
+dc = zeros(length(recs), 1);
+nr = zeros(length(recs), 1);
 for k = 1:length(recs)
     [~, fs] = audioread([base_path, recs{k}]);
     dummy = load([base_path, recs{k}(1:end-4), '/samples.mat']);
     dummy2 = [dummy.samples.active, dummy.samples.passive];
     samples{k} = dummy2 / fs;
     call_dur{k} = max(dummy2) / fs;
+    
+    p_rate(k) = length(dummy2) / (max(dummy2) / fs);
+    dc(k) = length(samples{k}) * 0.0005 / max(samples{k}); 
+    nr(k) = length(dummy2);
 end
+
+%% Pulse rate
+
 
 %% Save for Python
 save('/media/nils/Data/Moth/CallStats/CallSeries_Stats/samples.mat', 'samples');
