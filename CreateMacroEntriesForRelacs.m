e1b8cd7c2668702b87cfca12b5379618ed2a3624
NilsBrehm <nils.brehm@student.uni-tuebingen.de>
2018-12-03 14:45:08 +0100

    Remove not related files from main git

--- CreateMacroEntriesForRelacs.m
+++
@@ -1,7 +0,0 @@
-% Create macros entries for relacs
-clc
-for i = 1:length(stims)
-    text = ['SingleStimulus: stimfile=naturalmothcalls/', stims(i).name,...
-        '; waveform=From file; type=Wave; stimscale=true; stimampl=1; intensity=$intensity; duration=0; pause=10ms; ramp=0ms;side = $side; repeats = $repeats;carrierfreq = $carrierfreq;'];
-    disp(text)
-end
--- wienerentropy.m
+++
@@ -1,7 +0,0 @@
-for i = 1:size(pulses.active, 2)
-        spfA(i) = log(spectral_flatness(pulses.active(:, i)));
-end
-
-for i = 1:size(pulses.passive, 2)
-        spfP(i) = log(spectral_flatness(pulses.passive(:, i)));
-end
