80b9395716e9297348c538aba1fb42e63b5b804e
NilsBrehm <nils.brehm@student.uni-tuebingen.de>
2018-12-03 16:45:44 +0100

    removed files

--- cutout_parts.m
+++
@@ -1,7 +0,0 @@
-samplingrate = 480 * 1000;
-part = data(q8.DataIndex:q9.DataIndex);
-
-audio_path = [path, file(1:end-4)];
-mkdir(audio_path);
-
-audiowrite([audio_path, '\part_08','.wav'],part ,samplingrate)
